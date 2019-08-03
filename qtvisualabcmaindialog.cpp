2c9cb927d02727727a5c631aed00955a548482a6
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-03 14:04:42 +0200

    Remove pragma's

--- qtvisualabcmaindialog.cpp
+++ qtvisualabcmaindialog.cpp
@@ -3,8 +3,8 @@
 
 #pragma GCC diagnostic push
 
-#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
-#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
+
+
 #include <boost/lexical_cast.hpp>
 
 #include <QDesktopWidget>
